552a56ada237011d34054e5b316a6f2342aaa7fa
igorsk1 <igor.sniegurov@gmail.com>
2024-12-19 03:02:49 +0200

    feat: add calculateBalance, generateOdds tasks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -309,10 +309,12 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce(
+    (balance, [debit, credit]) => balance + (debit - credit),
+    0
+  );
 }
-
 /**
  * Breaks an array into chunks of the specified size.
  *
@@ -341,8 +343,8 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len }, (_, i) => i * 2 + 1);
 }
 
 /**
